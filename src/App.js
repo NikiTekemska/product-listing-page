24b8df2c467b3cd106f33c4cdf61080203750ecd
Nikoleta Tekemska <nikoletatt94@gmail.com>
2023-08-02 20:14:51 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+import { Header } from './components/Header';
+import { Filter } from './components/Filter';
+
+function App() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:3000/products.json`)
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+        })
+  },[]);
+
+  return (
+    <div className="App">
+      < Header />
+      <Filter />
+    </div>
+  );
+}
+
+export default App;
