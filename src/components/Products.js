6de5181c9cba67dec62814f31d9d8882f3b9c3ed
Nikoleta Tekemska <nikoletatt94@gmail.com>
2023-08-06 19:27:44 +0300

    Update Products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -13,13 +13,28 @@ export const Products = () =>{
         )
   },[category]);
   console.log(categoryProducts);
+  const openFilter = () => {
+
+  }
 
   return (
      <>
          <div>
              <h1>{category}</h1>
+             <button onClick={openFilter}>Open filter</button>
+             <div>
+             <label htmlFor="sort">Sort</label>
+             <select name="sort" >
+             <option value="a-z">Alphabetical a-z</option>
+             <option value="z-a">Alphabetical z-a</option>
+             <option value="price-ascending">Price ascending</option>
+             <option value="price-descending">Price descending</option>
+             </select>
+             
+             </div>
+             
             {/* <img className={styles.categoryImg} src={categoryRecipes.image} alt="img" /> */}
-             {/* <h3>Best way to finish your meal.</h3> */}
+             
          </div>
              <div>{categoryProducts.map(x => <ProductsCard key={x._id} {...x} />)}</div>
         
