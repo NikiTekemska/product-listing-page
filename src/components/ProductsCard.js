f115198900a6d37a2c320a2a61ca58c17054d36d
Nikoleta Tekemska <nikoletatt94@gmail.com>
2023-08-06 19:27:49 +0300

    Update ProductsCard.js

--- src/components/ProductsCard.js
+++ src/components/ProductsCard.js
@@ -2,14 +2,17 @@ import styles from './ProductsCard.module.css'
 export const ProductsCard = ({
     brand,
     img,
-    price
+    price,
+    description
 }) => {
     return (
         <ul className={styles.ProductsCard}>
             <li>
                 <h1>{brand}</h1>
                 <img src={img} alt="img" />
-                <h3>{price}</h3>
+                <h3>Price:{price}$</h3>
+                <h5>Description:{description}</h5>
+                <button>Add to cart</button>
             </li>
         </ul>
 
