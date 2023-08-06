8fb3a35fb1dec8b9e728a1efc7ea88ca36b52e3f
Nikoleta Tekemska <nikoletatt94@gmail.com>
2023-08-06 15:35:43 +0300

    Update Products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,29 @@
 import { useState, useEffect } from 'react';
-const [products, setProducts] = useState([]);
+import { useParams } from 'react-router-dom';
+import { ProductsCard } from './ProductsCard';
+
+export const Products = () =>{
+    const [categoryProducts, setCategoryProducts] = useState([]);
+    const { category } = useParams();
   useEffect(() => {
     fetch(`http://localhost:3000/products.json`)
         .then(res => res.json())
-        .then(data => {
-          setProducts(data.products)
-        })
-  },[]);
- console.log(products)
+        .then(data => 
+            setCategoryProducts(data[category])
+        )
+  },[category]);
+  console.log(categoryProducts);
+
+  return (
+     <>
+         <div>
+             <h1>{category}</h1>
+            {/* <img className={styles.categoryImg} src={categoryRecipes.image} alt="img" /> */}
+             {/* <h3>Best way to finish your meal.</h3> */}
+         </div>
+             <div>{categoryProducts.map(x => <ProductsCard key={x._id} {...x} />)}</div>
+        
+
+     </>
+ )
+}
